bd838aeda3ff8394b2608093eacef6b4d7b82b2a
Steve Ngueneko <steve.ngueneko@foyang-tech.de>
2019-08-12 00:50:02 +0200

    - update UserController path and add new page to creat eedit an show profile of the user

--- src/main/java/de/foyangtech/ecommerce/catalogmanager/controller/UserController.java
+++ src/main/java/de/foyangtech/ecommerce/catalogmanager/controller/UserController.java
@@ -3,11 +3,14 @@ package de.foyangtech.ecommerce.catalogmanager.controller;
 import de.foyangtech.ecommerce.catalogmanager.persistance.dao.ProductDao;
 import de.foyangtech.ecommerce.catalogmanager.persistance.dao.UserDao;
 import de.foyangtech.ecommerce.catalogmanager.persistance.model.User;
+import de.foyangtech.ecommerce.catalogmanager.service.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -43,9 +46,25 @@ public class UserController {
         return new ResponseEntity<>(userByUsername, HttpStatus.OK);
     }
 
-    @GetMapping("/create_user")
-    public String showCreateForm() {
-        return "create_user";
+    @GetMapping("/create")
+    public String showCreateForm(@AuthenticationPrincipal UserPrincipal user, Model model) {
+
+        model.addAttribute("user", user);
+        return "user/user_create";
+    }
+
+    @GetMapping("/profile")
+    public String showUserProfile(@AuthenticationPrincipal UserPrincipal user, Model model) {
+
+        model.addAttribute("user", user);
+        return "user/user_profile";
+    }
+
+    @GetMapping("/edit")
+    public String showUserEdit(@AuthenticationPrincipal UserPrincipal user, Model model) {
+
+        model.addAttribute("user", user);
+        return "user/user_edit";
     }
 
 }
